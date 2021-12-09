b6b7ed041374671b53f8fa0f162846833e829f02
sudeeps91 <sudeeps007@gmail.com>
2021-12-09 09:08:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-
-echo "<h1>Hello World from $instanceId </h1>" > /var/www/html/index.html
+#instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+containerId=$(hostname)
+echo "<h1>Hello World from $hostname </h1>" > /var/www/html/index.html
