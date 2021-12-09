c5c60d879d4fb16ef62d37f33c0d5db507218a71
sudeeps91 <sudeeps007@gmail.com>
2021-12-09 17:20:52 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-containerId=$(hostname)
-echo "<h1>Hello World from $containerId </h1>" > /usr/share/nginx/html/index.html
